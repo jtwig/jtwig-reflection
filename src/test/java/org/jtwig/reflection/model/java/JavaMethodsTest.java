543735358e40b98f23e507ba47023b4cdffb7f90
Casey Brooks <cjbrooks12@gmail.com>
2017-09-23 14:22:47 -0500

    Adds all methods, not just declared methods. Accounts for default interface methods and methods on Object class

--- src/test/java/org/jtwig/reflection/model/java/JavaMethodsTest.java
+++ src/test/java/org/jtwig/reflection/model/java/JavaMethodsTest.java
@@ -2,8 +2,8 @@ package org.jtwig.reflection.model.java;
 
 import org.junit.Test;
 
-import static org.jtwig.reflection.model.java.JavaClassManager.classManager;
-import static org.junit.Assert.assertEquals;
+import static org.jtwig.reflection.model.java.JavaClassManager.*;
+import static org.junit.Assert.*;
 
 public class JavaMethodsTest {
     @Test
@@ -25,6 +25,44 @@ public class JavaMethodsTest {
         assertEquals(javaMethods.getMethod(Object.class).get().getNative(), ExtendedBeanTest.class.getDeclaredMethod("asd", Object.class));
     }
 
+    @Test
+    public void getMethodImplemented() throws Exception {
+        JavaMethods javaMethods = classManager().metadata(ImplementedBeanTest.class).method("asd");
+
+        assertEquals(javaMethods.getMethod().get().getNative(), TestInterface.class.getDeclaredMethod("asd"));
+        assertEquals(javaMethods.getMethod(String.class).get().getNative(), TestInterface.class.getDeclaredMethod("asd", String.class));
+        assertEquals(javaMethods.getMethod(String.class, Object.class).get().getNative(), TestInterface.class.getDeclaredMethod("asd", String.class, Object.class));
+        assertEquals(javaMethods.getMethod(Object.class).get().getNative(), ImplementedBeanTest.class.getDeclaredMethod("asd", Object.class));
+    }
+
+    @Test
+    public void objectGetClass() throws Exception {
+        // getClass
+        JavaMethods javaMethods = classManager().metadata(BeanTest.class).method("getClass");
+        assertEquals(javaMethods.getMethod().get().getNative(), Object.class.getDeclaredMethod("getClass"));
+    }
+
+    @Test
+    public void objectEquals() throws Exception {
+        // equals
+        JavaMethods javaMethods = classManager().metadata(BeanTest.class).method("equals");
+        assertEquals(javaMethods.getMethod(Object.class).get().getNative(), Object.class.getDeclaredMethod("equals", Object.class));
+    }
+
+    @Test
+    public void objectHashCode() throws Exception {
+        // hashcode
+        JavaMethods javaMethods = classManager().metadata(BeanTest.class).method("hashCode");
+        assertEquals(javaMethods.getMethod().get().getNative(), Object.class.getDeclaredMethod("hashCode"));
+    }
+
+    @Test
+    public void objectToString() throws Exception {
+        // toString
+        JavaMethods javaMethods = classManager().metadata(BeanTest.class).method("toString");
+        assertEquals(javaMethods.getMethod().get().getNative(), Object.class.getDeclaredMethod("toString"));
+    }
+
     public static class BeanTest {
         private void asd () {}
         private void asd (String asd) {}
@@ -32,6 +70,16 @@ public class JavaMethodsTest {
     }
 
     public static class ExtendedBeanTest extends BeanTest {
+        private void asd (Object ob) {}
+    }
+
+    public interface TestInterface {
+        default void asd () {}
+        default void asd (String asd) {}
+        default void asd (String asd, Object object) {}
+    }
+
+    public static class ImplementedBeanTest implements TestInterface {
         private void asd (Object ob) {}
     }
 }
